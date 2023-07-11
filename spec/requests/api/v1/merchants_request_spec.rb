841fe34505258dd87c72333fca0f5e82e8f65f38
Michael Callahan <mcalla242@gmail.com>
2023-07-11 07:34:20 -0600

    Add testing specs for specific Merchant API request

--- spec/requests/api/v1/merchants_request_spec.rb
+++ spec/requests/api/v1/merchants_request_spec.rb
@@ -6,7 +6,7 @@ describe 'Merchants API' do
     it 'can get call merchants' do
       create_list(:merchant, 5)
 
-      get '/api/v1/merchants'
+      get api_v1_merchants_path
 
       expect(response).to be_successful
 
@@ -21,6 +21,27 @@ describe 'Merchants API' do
         expect(merchant).to have_key(:name)
         expect(merchant[:name]).to be_a(String)
       end
+    end
+  end
+
+  context 'GET one merchant' do
+    it 'can get one merchant by id' do
+      id = create(:merchant).id
+      
+      get api_v1_merchant(id)
+
+      expect(response).to be_successful
+
+      merchant = JSON.parse(response.body, symbolize_names: true)
+
+      expect(merchant.count).to eq(1)
+
+
+      expect(merchant).to have_key(:id)
+      expect(merchant[:id]).to be_an(Integer)
+      
+      expect(merchant).to have_key(:name)
+      expect(merchant[:name]).to be_a(String)
     end
   end
 end
