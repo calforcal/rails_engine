dece4d3241c34a7d350e1fa79197b1640c1f092e
Michael Callahan <mcalla242@gmail.com>
2023-07-11 13:33:46 -0600

    Add testing specs for merchants items request

--- spec/requests/api/v1/merchants_request_spec.rb
+++ spec/requests/api/v1/merchants_request_spec.rb
@@ -43,4 +43,36 @@ describe 'Merchants API' do
       expect(merchant[:attributes][:name]).to be_a(String)
     end
   end
+
+  context 'GET one merchants items' do
+    let!(:merchant) { create(:merchant) }
+    let!(:item1) { create(:item, merchant_id: merchant.id) }
+    let!(:item2) { create(:item, merchant_id: merchant.id) }
+    let!(:item3) { create(:item, merchant_id: merchant.id) }
+
+    it 'can get ALL of one merchants items' do
+      get api_v1_merchant_items_path(merchant)
+
+      expect(response).to be_successful
+
+      parsed = JSON.parse(response.body, symbolize_names: true)
+      items = parsed[:data]
+
+      expect(items).to have_key(:id)
+      expect(items[:id]).to be_a(String)
+      
+      expect(items[:attributes]).to have_key(:name)
+      expect(items[:attributes][:name]).to be_a(String)
+
+      expect(items[:attributes]).to have_key(:description)
+      expect(items[:attributes][:description]).to be_a(String)
+
+      expect(items[:attributes]).to have_key(:unit_price)
+      expect(items[:attributes][:unit_price]).to be_a(String)
+
+      expect(items[:attributes]).to have_key(:merchant_id)
+      expect(items[:attributes][:merchant_id]).to be_a(String)
+      expect(items[:attributes][:merchant_id]).to eq(merchant.id)
+    end
+  end
 end
