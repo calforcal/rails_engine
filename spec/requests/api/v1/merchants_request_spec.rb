50c03b559313104ec2c3bb9226543c8401f03bd6
Michael Callahan <mcalla242@gmail.com>
2023-07-11 13:47:50 -0600

    Add functionality to support a Merchants Items API request

--- spec/requests/api/v1/merchants_request_spec.rb
+++ spec/requests/api/v1/merchants_request_spec.rb
@@ -58,21 +58,23 @@ describe 'Merchants API' do
       parsed = JSON.parse(response.body, symbolize_names: true)
       items = parsed[:data]
 
-      expect(items).to have_key(:id)
-      expect(items[:id]).to be_a(String)
-      
-      expect(items[:attributes]).to have_key(:name)
-      expect(items[:attributes][:name]).to be_a(String)
-
-      expect(items[:attributes]).to have_key(:description)
-      expect(items[:attributes][:description]).to be_a(String)
-
-      expect(items[:attributes]).to have_key(:unit_price)
-      expect(items[:attributes][:unit_price]).to be_a(String)
-
-      expect(items[:attributes]).to have_key(:merchant_id)
-      expect(items[:attributes][:merchant_id]).to be_a(String)
-      expect(items[:attributes][:merchant_id]).to eq(merchant.id)
+      items.each do |item|
+        expect(item).to have_key(:id)
+        expect(item[:id]).to be_a(String)
+        
+        expect(item[:attributes]).to have_key(:name)
+        expect(item[:attributes][:name]).to be_a(String)
+        
+        expect(item[:attributes]).to have_key(:description)
+        expect(item[:attributes][:description]).to be_a(String)
+        
+        expect(item[:attributes]).to have_key(:unit_price)
+        expect(item[:attributes][:unit_price]).to be_a(Float)
+        
+        expect(item[:attributes]).to have_key(:merchant_id)
+        expect(item[:attributes][:merchant_id]).to be_a(Integer)
+        expect(item[:attributes][:merchant_id]).to eq(merchant.id)
+      end
     end
   end
 end
