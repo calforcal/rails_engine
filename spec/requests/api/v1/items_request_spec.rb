148bbb3d3f9c24826044f7f06e08bce003307405
Michael Callahan <mcalla242@gmail.com>
2023-07-13 06:33:32 -0600

    Refactor merchant test specs to test specific data

--- spec/requests/api/v1/items_request_spec.rb
+++ spec/requests/api/v1/items_request_spec.rb
@@ -22,21 +22,22 @@ describe 'Items API' do
       tire = items[1]
       helmet = items[2]
       
-
-      expect(bike).to have_key(:id)
-      expect(bike[:id]).to be_a(String)
-      
-      expect(bike[:attributes]).to have_key(:name)
-      expect(bike[:attributes][:name]).to be_a(String)
-      
-      expect(bike[:attributes]).to have_key(:description)
-      expect(bike[:attributes][:description]).to be_a(String)
-      
-      expect(bike[:attributes]).to have_key(:unit_price)
-      expect(bike[:attributes][:unit_price]).to be_a(Float)
-      
-      expect(bike[:attributes]).to have_key(:merchant_id)
-      expect(bike[:attributes][:merchant_id]).to be_a(Integer)
+      items.each do |item|
+        expect(item).to have_key(:id)
+        expect(item[:id]).to be_a(String)
+        
+        expect(item[:attributes]).to have_key(:name)
+        expect(item[:attributes][:name]).to be_a(String)
+        
+        expect(item[:attributes]).to have_key(:description)
+        expect(item[:attributes][:description]).to be_a(String)
+        
+        expect(item[:attributes]).to have_key(:unit_price)
+        expect(item[:attributes][:unit_price]).to be_a(Float)
+        
+        expect(item[:attributes]).to have_key(:merchant_id)
+        expect(item[:attributes][:merchant_id]).to be_a(Integer)
+      end
 
       expect(bike[:attributes][:name]).to eq('Bike')
       expect(bike[:attributes][:description]).to eq('It rolls good')
--- spec/requests/api/v1/merchants_request_spec.rb
+++ spec/requests/api/v1/merchants_request_spec.rb
@@ -1,10 +1,13 @@
 require 'rails_helper'
 
 describe 'Merchants API' do
-  
+
+  let!(:merchant1) { create(:merchant, name: 'Bike Shop') }
+  let!(:merchant2) { create(:merchant, name: 'Tacos') }
+  let!(:merchant3) { create(:merchant, name: 'Truck Stop') }
+
   context 'GETs all merchants' do
     it 'can get call merchants' do
-      create_list(:merchant, 5)
 
       get api_v1_merchants_path
 
@@ -13,7 +16,11 @@ describe 'Merchants API' do
       parsed = JSON.parse(response.body, symbolize_names: true)
       merchants = parsed[:data]
 
-      expect(merchants.count).to eq(5)
+      expect(merchants.count).to eq(3)
+
+      bike = merchants[0]
+      taco = merchants[1]
+      truck = merchants[2]
 
       merchants.each do |merchant|
         expect(merchant).to have_key(:id)
@@ -22,14 +29,21 @@ describe 'Merchants API' do
         expect(merchant[:attributes]).to have_key(:name)
         expect(merchant[:attributes][:name]).to be_a(String)
       end
+
+      expect(bike[:id]).to eq(merchant1.id.to_s)
+      expect(bike[:attributes][:name]).to eq('Bike Shop')
+
+      expect(taco[:id]).to eq(merchant2.id.to_s)
+      expect(taco[:attributes][:name]).to eq('Tacos')
+
+      expect(truck[:id]).to eq(merchant3.id.to_s)
+      expect(truck[:attributes][:name]).to eq('Truck Stop')
     end
   end
 
   context 'GET one merchant' do
     it 'can get one merchant by id' do
-      id = create(:merchant).id
-
-      get api_v1_merchant_path(id)
+      get api_v1_merchant_path(merchant1)
 
       expect(response).to be_successful
 
@@ -41,14 +55,16 @@ describe 'Merchants API' do
       
       expect(merchant[:attributes]).to have_key(:name)
       expect(merchant[:attributes][:name]).to be_a(String)
+
+      expect(merchant[:id]).to eq(merchant1.id.to_s)
+      expect(merchant[:attributes][:name]).to eq('Bike Shop')
     end
   end
 
   context 'GET one merchants items' do
     let!(:merchant) { create(:merchant) }
-    let!(:item1) { create(:item, merchant_id: merchant.id) }
-    let!(:item2) { create(:item, merchant_id: merchant.id) }
-    let!(:item3) { create(:item, merchant_id: merchant.id) }
+    let!(:item1) { create(:item, name: 'Bike', description: 'It rolls good', unit_price: 5000, merchant_id: merchant.id) }
+    let!(:item2) { create(:item, name: 'Tire', description: 'Helps with rolling', unit_price: 100,  merchant_id: merchant.id) }
 
     it 'can get ALL of one merchants items' do
       get api_v1_merchant_items_path(merchant)
@@ -57,6 +73,9 @@ describe 'Merchants API' do
 
       parsed = JSON.parse(response.body, symbolize_names: true)
       items = parsed[:data]
+
+      bike = items[0]
+      tire = items[1]
 
       items.each do |item|
         expect(item).to have_key(:id)
@@ -73,8 +92,17 @@ describe 'Merchants API' do
         
         expect(item[:attributes]).to have_key(:merchant_id)
         expect(item[:attributes][:merchant_id]).to be_a(Integer)
-        expect(item[:attributes][:merchant_id]).to eq(merchant.id)
       end
+
+      expect(bike[:attributes][:name]).to eq('Bike')
+      expect(bike[:attributes][:description]).to eq('It rolls good')
+      expect(bike[:attributes][:unit_price]).to eq(5000)
+      expect(bike[:attributes][:merchant_id]).to eq(merchant.id)
+
+      expect(tire[:attributes][:name]).to eq('Tire')
+      expect(tire[:attributes][:description]).to eq('Helps with rolling')
+      expect(tire[:attributes][:unit_price]).to eq(100)
+      expect(tire[:attributes][:merchant_id]).to eq(merchant.id)
     end
   end
 
